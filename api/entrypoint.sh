58679a70e26d97da43ff71d74fe018234ce41025
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2023-06-06 16:31:25 +0200

    api. refactor with new api in golang

--- api/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-mkdir -p /etc/openvpn/sockets
-
-cd /nethsec-api
-source /nethsec-api/bin/activate
-
-exec python3 api.py
---
+++ api/main.go
@@ -0,0 +1,107 @@
+/*
+ * Copyright (C) 2023 Nethesis S.r.l.
+ * http://www.nethesis.it - info@nethesis.it
+ *
+ * SPDX-License-Identifier: GPL-2.0-only
+ *
+ * author: Edoardo Spadoni <edoardo.spadoni@nethesis.it>
+ */
+
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+
+	"github.com/fatih/structs"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-contrib/gzip"
+	"github.com/gin-gonic/gin"
+
+	"github.com/NethServer/nethsecurity-api/logs"
+	"github.com/NethServer/nethsecurity-api/response"
+	"github.com/NethServer/nethsecurity-controller/api/configuration"
+	"github.com/NethServer/nethsecurity-controller/api/methods"
+	"github.com/NethServer/nethsecurity-controller/api/middleware"
+)
+
+// @title NethSecurity Controller API Server
+// @version 1.0
+// @description NethSecurity Controller API Server is used to manage multiple stand-alone NethSecurity instances
+// @termsOfService https://nethserver.org/terms/
+
+// @contact.name NethServer Developer Team
+// @contact.url https://nethserver.org/support
+
+// @license.name GNU GENERAL PUBLIC LICENSE
+
+// @host localhost:5000
+// @schemes http
+// @BasePath /api
+
+func main() {
+	// init logs with syslog
+	logs.Init("nethsecurity_controller")
+
+	// init configuration
+	configuration.Init()
+
+	// disable log to stdout when running in release mode
+	if gin.Mode() == gin.ReleaseMode {
+		gin.DefaultWriter = ioutil.Discard
+	}
+
+	// init routers
+	router := gin.Default()
+
+	// add default compression
+	router.Use(gzip.Gzip(gzip.DefaultCompression))
+
+	// cors configuration only in debug mode GIN_MODE=debug (default)
+	if gin.Mode() == gin.DebugMode {
+		// gin gonic cors conf
+		corsConf := cors.DefaultConfig()
+		corsConf.AllowHeaders = []string{"Authorization", "Content-Type", "Accept"}
+		corsConf.AllowAllOrigins = true
+		router.Use(cors.New(corsConf))
+	}
+
+	// define api group
+	api := router.Group("/api")
+
+	// define login and logout endpoint
+	api.POST("/login", middleware.InstanceJWT().LoginHandler)
+	api.POST("/logout", middleware.InstanceJWT().LogoutHandler)
+
+	// define server registration, in waiting list
+	//api.POST("/units/register", methods.RegisterUnit)
+
+	// define JWT middleware
+	api.Use(middleware.InstanceJWT().MiddlewareFunc())
+	{
+		// refresh handler
+		api.GET("/refresh", middleware.InstanceJWT().RefreshHandler)
+
+		// units APIs
+		units := api.Group("/units")
+		{
+			units.GET("", methods.GetUnits)
+			// units.GET("/:unit_id", methods.GetUnit)
+			// units.GET("/:unit_id/token", methods.GetToken)
+			// units.POST("", methods.AddUnit)
+			// units.DELETE("/:unit_id", methods.DeleteUnit)
+		}
+	}
+
+	// handle missing endpoint
+	router.NoRoute(func(c *gin.Context) {
+		c.JSON(http.StatusNotFound, structs.Map(response.StatusNotFound{
+			Code:    404,
+			Message: "API not found",
+			Data:    nil,
+		}))
+	})
+
+	// run server
+	router.Run(configuration.Config.ListenAddress)
+}
