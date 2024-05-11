5a23da2113b018d69e609b4f4c16c9842839ff49
gsanchietti <gsanchietti@users.noreply.github.com>
2024-05-11 00:56:39 +0000

    build(deps): ns-ui package: update ns-ui package (automated)

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ repobase="ghcr.io/nethserver"
 
 images=()
 container=$(buildah from docker.io/alpine:3.16)
-ui_version="0.72.0"
+ui_version="0.74.0"
 
 trap "buildah rm ${container} ${container_api} ${container_proxy}" EXIT
 
