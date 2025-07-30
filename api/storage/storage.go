NethServer/nethsecurity-controller

commit 516fd3ad2c0599a920b1a7b9244e901b5f557d9d

fix(migration): check for accounts table existence
Just log an INFO message if accounts table does not exists:
it can happen after a migration because the SQLite database exists
but is empty

api/storage/storage.go
@@ -116,7 +116,17 @@ func MigrateUsersFromSqliteToPostgres(units []string) {
 	}
 	defer sqliteDB.Close()
 
-	// 3. Check if SQLite has users
+	// 3. Check if SQLite exists and has users
+	var tableExists bool
+	err = sqliteDB.QueryRow("SELECT name FROM sqlite_master WHERE type='table' AND name='accounts'").Scan(&tableExists)
+	if err != nil && err != sql.ErrNoRows {
+		logs.Logs.Println("[ERR][MIGRATION] error checking accounts table in SQLite: " + err.Error())
+		return
+	}
+	if !tableExists {
+		logs.Logs.Println("[INFO][MIGRATION] accounts table does not exist in SQLite: skipping user migration")
+		return
+	}
 	rows, err := sqliteDB.Query("SELECT id, username, password, display_name, created FROM accounts")
 	if err != nil {
 		logs.Logs.Println("[ERR][MIGRATION] cannot query SQLite accounts: " + err.Error())

commit c2293cb6b1176a41b16a7e86891bdbc60481520f

fix(api): prevent error on accounts migration
Fix regression introduced by 516fd3ad2c0599a920b1a7b9244e901b5f557d9d

api/storage/storage.go
@@ -118,7 +118,7 @@ func MigrateUsersFromSqliteToPostgres(units []string) {
 
 	// 3. Check if SQLite exists and has users
 	var tableExists bool
-	err = sqliteDB.QueryRow("SELECT name FROM sqlite_master WHERE type='table' AND name='accounts'").Scan(&tableExists)
+	err = sqliteDB.QueryRow("SELECT EXISTS (SELECT name FROM sqlite_master WHERE type='table' AND name='accounts')").Scan(&tableExists)
 	if err != nil && err != sql.ErrNoRows {
 		logs.Logs.Println("[ERR][MIGRATION] error checking accounts table in SQLite: " + err.Error())
 		return