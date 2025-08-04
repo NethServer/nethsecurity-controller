3b5f169037f8b1d292ddfa188b0d21f5f6a40f87
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2025-08-04 14:32:24 +0200

    fix(api): refactor migration process
    Changes:
    - execute the migration only if there are CCD files
    - add delete constraints to openvpn_config and wan_config tables
    - remove from database all units that do not have a CCD file:
      avoid showing ghost units after migration
    - chore: migration functions are now private

--- api/storage/upgrade_schema.sql
+++ api/storage/upgrade_schema.sql
@@ -12,3 +12,26 @@ ALTER TABLE units ADD COLUMN IF NOT EXISTS info JSONB;
 ALTER TABLE units ADD COLUMN IF NOT EXISTS updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
 ALTER TABLE units ADD COLUMN IF NOT EXISTS vpn_address TEXT;
 ALTER TABLE units ADD COLUMN IF NOT EXISTS vpn_connected_since TIMESTAMP NULL;
+/* Add missing constraints to existing tables */
+DO $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1
+        FROM information_schema.table_constraints
+        WHERE constraint_name = 'fk_unit'
+          AND table_name = 'openvpn_config'
+    ) THEN
+        ALTER TABLE openvpn_config
+            ADD CONSTRAINT fk_unit FOREIGN KEY(uuid) REFERENCES units(uuid) ON DELETE CASCADE;
+    END IF;
+
+    IF NOT EXISTS (
+        SELECT 1
+        FROM information_schema.table_constraints
+        WHERE constraint_name = 'fk_unit'
+          AND table_name = 'wan_config'
+    ) THEN
+        ALTER TABLE wan_config
+            ADD CONSTRAINT fk_unit FOREIGN KEY(uuid) REFERENCES units(uuid) ON DELETE CASCADE;
+    END IF;
+END$$;
