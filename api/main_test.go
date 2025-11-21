27625da75ec561c09593782bcb947ccc1e09274b
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2025-11-21 09:09:10 +0100

    fix: fix test headers

--- api/main_test.go
+++ api/main_test.go
@@ -1,3 +1,12 @@
+/*
+ * Copyright (C) 2025 Nethesis S.r.l.
+ * http://www.nethesis.it - info@nethesis.it
+ *
+ * SPDX-License-Identifier: GPL-2.0-only
+ *
+ * author: Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
+ */
+
 package main
 
 import (
